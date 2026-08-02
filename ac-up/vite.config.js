3533728631472dda22e513313f4d98eab9165842
clannina <acurro1981@gmail.com>
2026-08-02 21:47:20 +0200

    Add files via upload

--- ac-up/vite.config.js
+++ ac-up/vite.config.js
@@ -8,6 +8,9 @@ export default defineConfig({
     VitePWA({
       registerType: "autoUpdate",
       includeAssets: ["favicon.svg"],
+      strategies: "injectManifest",
+      srcDir: "src",
+      filename: "sw.js",
       manifest: {
         name: "AC UP",
         short_name: "AC UP",
@@ -22,7 +25,7 @@ export default defineConfig({
           { src: "icons/icon-512.png", sizes: "512x512", type: "image/png", purpose: "maskable" },
         ],
       },
-      workbox: {
+      injectManifest: {
         globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
       },
     }),
