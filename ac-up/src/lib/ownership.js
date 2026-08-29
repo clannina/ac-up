376fab026d8a3b739abcd6ccb04e1a7589c6ea11
clannina <acurro1981@gmail.com>
2026-08-29 23:39:33 +0200

    Add files via upload

---
+++ ac-up/src/lib/ownership.js
@@ -0,0 +1,3 @@
+// ID utente del "proprietario" principale (Anna): chiunque altro effettui l'accesso
+// vede solo AcPepe, non AC UP e AC Home.
+export const PROPRIETARIO_ID = "b0f37a79-06d3-4b61-b820-d1b7b51efad3";
